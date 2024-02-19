b30a1b9d77899292d2180db80f1d3dfb656200cb
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-02-19 13:41:50 +0530

    update

--- Vid88/main.js
+++ Vid88/main.js
@@ -1,8 +1,33 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World2!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello about us!')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact me!')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello blog!')
+})
+
+app.get('/blog/intro-to-js', (req, res) => {
+    // logic to fetch intro to js from the db
+    res.send('Hello intro to js!')
+})
+
+app.get('/blog/intro-to-python', (req, res) => {
+    // logic to fetch intro to python from the db
+    res.send('Hello intro to python!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
