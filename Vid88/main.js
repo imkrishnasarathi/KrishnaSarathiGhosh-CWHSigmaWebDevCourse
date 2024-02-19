df046353066200e951ca5e0dc84e8a5d836ce78a
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-02-19 13:58:23 +0530

    update

--- Vid88/main.js
+++ Vid88/main.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.send('Hello World2!')
 })
@@ -18,15 +20,22 @@ app.get('/blog', (req, res) => {
     res.send('Hello blog!')
 })
 
-app.get('/blog/intro-to-js', (req, res) => {
-    // logic to fetch intro to js from the db
-    res.send('Hello intro to js!')
+app.get('/blog/:slug', (req, res) => {
+    // logic to fetch {slug} from the db
+    console.log(req.params)
+    console.log(req.query)
+    res.send(`Hello ${req.params.slug}`)
 })
 
-app.get('/blog/intro-to-python', (req, res) => {
-    // logic to fetch intro to python from the db
-    res.send('Hello intro to python!')
-})
+// app.get('/blog/intro-to-js', (req, res) => {
+//     // logic to fetch intro to js from the db
+//     res.send('Hello intro to js!')
+// })
+//
+// app.get('/blog/intro-to-python', (req, res) => {
+//     // logic to fetch intro to python from the db
+//     res.send('Hello intro to python!')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
