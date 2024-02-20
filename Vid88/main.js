858a72af6f3791d594dc03dde5402f87bac6e3e5
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-02-20 14:05:15 +0530

    update

--- Vid88/main.js
+++ Vid88/main.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-
 app.get('/', (req, res) => {
     res.send('Hello World2!')
 })
--- Vid89/main.js
+++ Vid89/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const blog = require('./routes/blog')
 
+app.use('/blog', blog)
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -14,6 +16,16 @@ app.get('/', (req, res) => {
     console.log("hey it's a put request")
     res.send('Got a PUT request at /')
 })
+
+app.get("/index", (req, res) => {
+    console.log("Hey it's index")
+    res.sendFile('templates/index.html', {root: __dirname})
+})
+
+app.get("/api", (req, res) => {
+    res.json({a: 1, b: 2, c: 3, name: ["Harry", "Krish", "Ron"]})
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
