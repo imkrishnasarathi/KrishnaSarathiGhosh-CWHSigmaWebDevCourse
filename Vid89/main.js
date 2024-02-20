09c36b7488d3b454bd47f6254b2fa9077bada814
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-02-20 13:41:32 +0530

    update

--- Vid89/main.js
+++ Vid89/main.js
@@ -7,14 +7,10 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     console.log("Hey it's a get request")
     res.send('Hello World2!')
-})
-
-app.post('/', (req, res) => {
+}).post('/', (req, res) => {
     console.log("Hey it's a post request")
     res.send('Got a POST request')
-})
-
-app.put('/', (req, res) => {
+}).put('/', (req, res) => {
     console.log("hey it's a put request")
     res.send('Got a PUT request at /')
 })
