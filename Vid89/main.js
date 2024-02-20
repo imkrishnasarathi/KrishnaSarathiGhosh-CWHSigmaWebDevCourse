72bceca02bf22be80c5132a2029a647cf984b4d4
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-02-20 13:34:11 +0530

    update

--- Vid89/main.js
+++ Vid89/main.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log("Hey it's a get request")
+    res.send('Hello World2!')
+})
+
+app.post('/', (req, res) => {
+    console.log("Hey it's a post request")
+    res.send('Got a POST request')
 })
 
 app.listen(port, () => {
